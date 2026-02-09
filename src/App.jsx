eb3a20983e933637d08fba3b8d25df9df6959055
mubeen207 <mubeenulislam207@gmail.com>
2026-02-09 14:53:15 +0500

    ticket managment system

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Cities from "./components/Cities";
+
+function App() {
+  // const getData = async () => {
+  //   const data = await Cities();
+  //   console.log(data);
+  // };
+
+  // getData();
+
+  return (<>
+  <Cities />
+  </>);
+}
+
+export default App;
---
+++ src/components/FireBase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAE37jgIzcp3cBJ1FhNTogR34WHO4YtthI",
+  authDomain: "ticket-management-system-4b4d3.firebaseapp.com",
+  projectId: "ticket-management-system-4b4d3",
+  storageBucket: "ticket-management-system-4b4d3.firebasestorage.app",
+  messagingSenderId: "586684307304",
+  appId: "1:586684307304:web:59ff15514c47c9363ea8dd",
+  measurementId: "G-NVX05J5VLE",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
